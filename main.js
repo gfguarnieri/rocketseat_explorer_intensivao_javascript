40fc3803ddbe6bb13e6a96f178da23abcbfa3cb9
Giovanni Guarnieri <gfguarnieri@hotmail.com>
2024-06-12 00:26:57 -0300

    :sparkles: feat: simple exercises - JS

---
+++ main.js
@@ -0,0 +1,47 @@
+console.log('Hello World!');
+
+let num1 = 10;
+let num2 = 20;
+console.log(num1 + num2);
+
+let num3 = 30;
+if(typeof num3 === 'number'){
+    console.log('É um número');
+}
+else{
+    console.log('Não é um número');
+}
+
+let str = 'string';
+if(typeof str === 'string'){
+    console.log('É uma string');
+}
+
+let bool = true;
+if(typeof bool === 'boolean'){
+    console.log('É um booleano');
+}
+
+let num4 = 40;
+let num5 = 50;
+console.log(num4 - num5);
+
+console.log(num4 * num5);
+
+console.log(num4 / num5);
+
+let num6 = 60;
+if(num6 % 2 === 0){
+    console.log('É um número par');
+}
+else{
+    console.log('Não é um número par');
+}
+
+let num7 = 70;
+if(num7 % 2 !== 0){
+    console.log('É um número ímpar');
+}
+else{
+    console.log('Não é um número ímpar');
+}
